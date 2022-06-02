b01b8ace25924994e0073a396075319e67a21058
root <santhoshkumar92272@gmail.com>
2022-06-02 12:12:45 +0000

    Conditional Rendering and Passing Default Properties

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import {Component} from 'react'
+
+import Welcome from './components/Welcome'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    isLoggedIn: false,
+  }
+
+  render() {
+    const {isLoggedIn} = this.state
+    return (
+      <div className="container">
+        <Welcome />
+        {isLoggedIn && <button>Logout</button>}
+        {!isLoggedIn && <button>LogIn</button>}
+      </div>
+    )
+  }
+}
+
+export default App
